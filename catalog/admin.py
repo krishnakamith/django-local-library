cfc551c49bf3163cfa7d29ab47e98b31d469035e
krishnakamith <krishnakamith@gmail.com>
2025-06-14 10:35:38 +0530

    added views

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Author, Book, BookInstance, Genre, Language
 
-# Register your models here.
+#admin.site.register(Book)
+#admin.site.register(Author)
+#admin.site.register(BookInstance)
+admin.site.register(Genre)
+admin.site.register(Language)
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = [('first_name', 'last_name'),('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+
+admin.site.register(Author, AuthorAdmin)
+
+class BookInstanceInline(admin.TabularInline):
+   model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display =('title', 'author', 'display_genre')
+
+    inlines = [BookInstanceInline]
+
+    def display_genre(self, obj):
+        return ','.join(genre.name for genre in obj.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_date', 'id')
+    list_filter = ('status', 'due_date')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_date')
+        }),
+    )
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact = 'a').count()
+    num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+    num_the_books = Book.objects.filter(title__iexact = "the").count()
+
+
+    context = {
+        'num_books' : num_books,
+        'num_instances' : num_instances,
+        'num_instances_available' : num_instances_available,
+        'num_authors' : num_authors,
+        'num_genres' : num_genres,
+        'num_the_books' : num_the_books,
+    }
+
+    return render(request, 'catalog/index.html', context=context)
+
+
+
+
+
