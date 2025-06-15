5359aa08f6a662efb11c2284a7980f25fa995dc1
krishnakamith <krishnakamith@gmail.com>
2025-06-15 14:26:48 +0530

    created website

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     
@@ -22,7 +23,18 @@ def index(request):
 
     return render(request, 'catalog/index.html', context=context)
 
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
 
+class BookDetailView(generic.DetailView):
+    model = Book
 
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    
 
 
